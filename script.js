928086eab521524ffbd1db9b90a6ce5f97b6de92
Dereck Sherrer <derecksherrer@yahoo.ca>
2020-11-25 07:37:11 -0500

    init - basic title page layout

---
+++ script.js
@@ -0,0 +1,8 @@
+function hideTitle() {
+    let titleScreen = document.getElementById("title-container");
+    let gameScreen = document.getElementById("game-container");
+    let startButton = document.getElementById("start-game");
+    titleScreen.style.display = "none";
+    gameScreen.style.display = "block";
+}
+
