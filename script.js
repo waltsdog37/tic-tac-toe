17cb7f65cb70e40fd56e15ced9a912eb05645cf2
Dereck Sherrer <derecksherrer@yahoo.ca>
2020-11-25 14:42:57 -0500

    title page layout, game board and mark layout

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
+// dummy game squares filled
+let gameBoard = {
+    ulq: "X",
+    ucq: "O",
+    urq: "X",
+    mlq: "O",
+    mcq: "X",
+    mrq: "O",
+    llq: "X",
+    lcq: "O",
+    lrq: "X"
+
+}
+
 function hideTitle() {
     let titleScreen = document.getElementById("title-container");
     let gameScreen = document.getElementById("game-container");
     let startButton = document.getElementById("start-game");
     titleScreen.style.display = "none";
-    gameScreen.style.display = "block";
+    gameScreen.style.display = "flex";
 }
 
+function updateGameBoard () {
+    let squares = document.querySelectorAll(".game-board");
+    for (let i = 0; i < 9; i++) {
+        let key = squares[i].id;
+        let mark = gameBoard[key];
+        squares[i].innerHTML = mark;
+
+        console.log(key);
+        console.log(mark);
+    }
+}
+
+updateGameBoard();
