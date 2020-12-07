1bb8601ee5f15f6693953872f9152b78e98a34b6
Dereck Sherrer <derecksherrer@yahoo.ca>
2020-12-06 19:49:30 -0500

    revamped JS, now using gameboard array

--- script.js
+++ script.js
@@ -1,35 +1,56 @@
-// dummy game squares filled
-let gameBoard = {
-    ulq: "X",
-    ucq: "O",
-    urq: "X",
-    mlq: "O",
-    mcq: "X",
-    mrq: "O",
-    llq: "X",
-    lcq: "O",
-    lrq: "X"
+// store blank gameboard in array
+let gameboard = ['','','','','','','','',''];
+const playerX = "X";
+const playerO = "O";
+let xTurn;
 
-}
+// DOM variables
+let errorMessage = document.getElementById("error-message");
+let titleScreen = document.getElementById("title-container");
+let gameScreen = document.getElementById("game-container");
+let squares = document.querySelectorAll(".game-board");
+let playerOne = document.getElementById("player-one");
+let playerTwo = document.getElementById("player-two");
 
+
+// listen for clicks on each square, prevent multi-click
+squares.forEach(cell => {
+    cell.addEventListener("click", markBoard, { once: true })
+});
+
+// hide title page on click and open game page
 function hideTitle() {
-    let titleScreen = document.getElementById("title-container");
-    let gameScreen = document.getElementById("game-container");
-    let startButton = document.getElementById("start-game");
     titleScreen.style.display = "none";
     gameScreen.style.display = "flex";
+    //getPlayers();
 }
 
-function updateGameBoard () {
-    let squares = document.querySelectorAll(".game-board");
-    for (let i = 0; i < 9; i++) {
-        let key = squares[i].id;
-        let mark = gameBoard[key];
-        squares[i].innerHTML = mark;
+// main click function
+function markBoard(e) {
+    let cell = e.target;
+    let currentTurn = xTurn ? playerO : playerX;
+    placeMark(cell, currentTurn);
+    // check for win
+    // check for draw
+    newTurn();
+}
 
-        console.log(key);
-        console.log(mark);
+// add mark to gameboard array and update gameboard
+function placeMark(cell, currentTurn){
+    let cellIndex = cell.getAttribute("data-index");
+    gameboard[cellIndex] = currentTurn;
+    console.log(gameboard);
+    renderMarks();
+}
+
+// fill in gameboard with marks from gameboard array
+function renderMarks() {
+    for (let i=0; i<9; i++) {
+        squares[i].innerHTML = gameboard[i];
     }
 }
 
-updateGameBoard();
+// change player after each new mark
+function newTurn() {
+    xTurn = !xTurn;
+}
